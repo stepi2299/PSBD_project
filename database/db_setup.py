d365118b2cd5179113ef2cfff027428caa4b57e6
maciej <maciekwolus@gmail.com>
2021-06-08 11:40:41 +0200

    Correcting some previous mistakes in creating tables

--- database/db_setup.py
+++ database/db_setup.py
@@ -4,77 +4,89 @@ import psycopg2
 def create_tables():
     commands = (
         """
-        CREATE TABLE place (
-            id_place SERIAL PRIMARY KEY,
-            adding_date DATE,
-            localisation_country VARCHAR(50) NOT NULL,
-            localisation_region VARCHAR(50) NOT NULL,
-            localisation_language VARCHAR(50) NOT NULL,
-            localisation_latitude REAL,
-            localisation_longitude REAL
+        CREATE TABLE photo (
+            id_photo SERIAL PRIMARY KEY,
+            file_size REAL,
+            file_path VARCHAR(200),
+            file_photo BYTEA
         )
         """,
         """
         CREATE TABLE hotel (
             id_hotel SERIAL PRIMARY KEY,
-            id_place INTEGER NOT NULL,
             link VARCHAR(200) NOT NULL,
             km_to_place REAL,
             address_city VARCHAR(100) NOT NULL,
             address_postal_code VARCHAR(200) NOT NULL,
             address_street VARCHAR(100) NOT NULL,
-            address_number VARCHAR(10) NOT NULL,
-            FOREIGN KEY (id_place)
-                REFERENCES place (id_place)
+            address_number VARCHAR(10) NOT NULL
         )
         """,
         """
         CREATE TABLE communication (
             id_communication SERIAL PRIMARY KEY,
-            id_place INTEGER NOT NULL,
             link VARCHAR(200) NOT NULL,
             km_to_place REAL,
             type VARCHAR(20) NOT NULL,
             address_city VARCHAR(100) NOT NULL,
             address_latitude REAL,
-            address_longitude REAL,
-            FOREIGN KEY (id_place)
-                REFERENCES place (id_place)
+            address_longitude REAL
         )
         """,
         """
         CREATE TABLE weather (
-            date DATE PRIMARY KEY,
-            id_place INTEGER NOT NULL,
+            weather_date DATE PRIMARY KEY,
             cloudy VARCHAR(20) NOT NULL,
             humidity REAL,
-            temperature REAL,
-            FOREIGN KEY (id_place)
-                REFERENCES place (id_place)
-                ON UPDATE CASCADE ON DELETE CASCADE
+            temperature REAL
         )
         """,
         """
         CREATE TABLE attraction (
             id_attraction SERIAL PRIMARY KEY,
-            id_place INTEGER NOT NULL,
+            id_photo INTEGER NOT NULL,
             type VARCHAR(50) NOT NULL,
             price REAL,
             description VARCHAR(500),
             open_hours VARCHAR(20) NOT NULL,
             link VARCHAR(200),
-            FOREIGN KEY (id_place)
-                REFERENCES place (id_place)
-                ON UPDATE CASCADE ON DELETE CASCADE
+            FOREIGN KEY (id_photo)
+                REFERENCES photo (id_photo)
+        )
+        """,
+        """
+        CREATE TABLE place (
+            id_place SERIAL PRIMARY KEY,
+            id_hotel INTEGER NOT NULL,
+            id_communication INTEGER NOT NULL,
+            id_attraction INTEGER NOT NULL,
+            weather_date DATE,
+            adding_date DATE,
+            localisation_country VARCHAR(50) NOT NULL,
+            localisation_region VARCHAR(50) NOT NULL,
+            localisation_language VARCHAR(50) NOT NULL,
+            localisation_latitude REAL,
+            localisation_longitude REAL,
+            FOREIGN KEY (id_hotel)
+                REFERENCES hotel (id_hotel),
+            FOREIGN KEY (id_communication)
+                REFERENCES communication (id_communication),
+            FOREIGN KEY (weather_date)
+                REFERENCES weather (weather_date),
+            FOREIGN KEY (id_attraction)
+                REFERENCES attraction (id_attraction)
         )
         """,
         """
         CREATE TABLE app_user (
             login VARCHAR(20) PRIMARY KEY,
+            id_photo INTEGER NOT NULL,
             password VARCHAR(20) NOT NULL,
             creation_date DATE,
             email VARCHAR(50) NOT NULL,
-            country VARCHAR(50) NOT NULL
+            country VARCHAR(50) NOT NULL,
+            FOREIGN KEY (id_photo)
+                REFERENCES photo (id_photo)
         )
         """,
         """
@@ -93,49 +105,32 @@ def create_tables():
         )INHERITS (app_user)
         """,
         """
+        CREATE TABLE comment (
+            id_comment SERIAL PRIMARY KEY,
+            login VARCHAR(20) NOT NULL,
+            add_date DATE,
+            content VARCHAR(500) NOT NULL,
+            FOREIGN KEY (login)
+                REFERENCES app_user (login)
+        )
+        """,
+        """
         CREATE TABLE post (
             id_post SERIAL PRIMARY KEY,
             id_place INTEGER NOT NULL,
             login VARCHAR(20) NOT NULL, 
+            id_comment INTEGER NOT NULL,
             add_date DATE,
+            id_photo INTEGER NOT NULL,
             review VARCHAR(500),
             FOREIGN KEY (id_place)
                 REFERENCES place (id_place),
             FOREIGN KEY (login)
-                REFERENCES app_user (login)
-        )
-        """,
-        """
-        CREATE TABLE photo (
-            id_photo SERIAL PRIMARY KEY,
-            id_attraction INTEGER NOT NULL,
-            id_post INTEGER NOT NULL,
-            login VARCHAR(20) NOT NULL,
-            file_size REAL,
-            file_path VARCHAR(200),
-            file_photo BYTEA,
-            FOREIGN KEY (id_attraction)
-                REFERENCES attraction (id_attraction)
-                ON UPDATE CASCADE ON DELETE CASCADE,
-            FOREIGN KEY (id_post)
-                REFERENCES post (id_post)
-                ON UPDATE CASCADE ON DELETE CASCADE,
-            FOREIGN KEY (login)
-                REFERENCES app_user (login)
-                ON UPDATE CASCADE ON DELETE CASCADE
-        )
-        """,
-        """
-        CREATE TABLE comment (
-            id_comment SERIAL PRIMARY KEY,
-            login VARCHAR(20) NOT NULL,
-            id_post INTEGER NOT NULL,
-            add_date DATE,
-            content VARCHAR(500) NOT NULL,
-            FOREIGN KEY (login)
                 REFERENCES app_user (login),
-            FOREIGN KEY (id_post)
-                REFERENCES post (id_post)
+            FOREIGN KEY (id_photo)
+                REFERENCES photo (id_photo),
+            FOREIGN KEY (id_comment)
+                REFERENCES comment (id_comment)
         )
         """,
         """
