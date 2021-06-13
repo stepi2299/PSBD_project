6abe66274916770e995135a0ec50e6b8b87f79b5
theczipson <krystekp131@interia.pl>
2021-06-13 23:27:44 +0200

    added inserts

--- database/db_client.py
+++ database/db_client.py
@@ -1,13 +1,12 @@
 import psycopg2
-from app import config
-
+#from app import config
 
 def making_connection():
-    return psycopg2.connect(host=config['DATABASE_HOST'],
-                            database=config['DATABASE_NAME'],
-                            user=config['DATABASE_USER'],
-                            password=config['DATABASE_PASSWORD'],
-                            port=config['DATABASE_PORT'])
+    return psycopg2.connect( host="localhost",
+            database="PSBD_places",
+            user="postgres",
+            password="postgres",
+            port=5432)
 
 
 # TODO check if this is working
@@ -34,8 +33,9 @@ def connect_and_pull_data(command, return_amount):
 
 
 # TODO check if this is working and how we can pass arguments to insert
-def connect_and_insert_data(command):
+def connect_and_insert_data(table_name, value):
     """ Connect to the PostgreSQL database server """
+    command = choosing_command(table_name)
     conn = None
     try:
         conn = making_connection()
@@ -44,7 +44,7 @@ def connect_and_insert_data(command):
         cur = conn.cursor()
 
         # execute statement
-        cur.execute(command)  # as a parameter SQL code
+        cur.execute(command, value)  # as a parameter SQL code
         print("Successfully executed SQL code")
         cur.close()
         conn.commit()
@@ -54,4 +54,125 @@ def connect_and_insert_data(command):
     finally:
         if conn is not None:
             conn.close()
+
+def choosing_command(key):
+    commands = {
+        "photo":            """INSERT INTO photo(name, 
+                                              file_size, 
+                                              file_path)
+                            VALUES(%s, %s, %s) RETURNING id_photo"""
+        ,
+        "hotel":            """INSERT INTO hotel(link, 
+                                              km_to_place, 
+                                              address_city, 
+                                              address_postal_code, 
+                                              address_street, 
+                                              address_number)
+                            VALUES(%s, %s, %s, %s, %s, %s) RETURNING id_hotel"""
+        ,
+        "communication":    """INSERT INTO communication(link,
+                                                        km_to_place,
+                                                        type,
+                                                        address_city,
+                                                        address_latitude,
+                                                        address_longitude)
+                            VALUES(%s, %s, %s, %s, %s, %s) RETURNING id_communication"""
+        ,
+        "attraction":       """"INSERT INTO attraction(id_photo,
+                                                     type,
+                                                     price,
+                                                     description,
+                                                     open_hours,
+                                                     link)
+                            VALUES(%s, %s, %s, %s, %s, %s) RETURNING id_attraction"""
+        ,
+        "place":            """"INSERT INTO place(id_hotel,
+                                                id_communication,
+                                                id_attraction,
+                                                adding_date,
+                                                localisation_country,
+                                                localisation_region,
+                                                localisation_language,
+                                                localisation_latitude,
+                                                localisation_longitude)
+                            VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s) RETURNING id_place"""
+        ,
+        "weather":          """INSERT INTO weather(weather_date,
+                                                  cloudy,
+                                                  humidity,
+                                                  temperature)
+                            VALUES(%s, %s, %s, %s, %s) RETURNING weather_id"""
+        ,
+        "app_user":         """INSERT INTO app_user(login,
+                                                   id_photo,
+                                                   password,
+                                                   creation_date,
+                                                   email,
+                                                   country)
+                            VALUES(%s, %s, %s, %s, %s, %s)"""
+        ,
+        "admin_place":      """INSERT INTO admin_place(login,
+                                                      id_photo,
+                                                      password,
+                                                      creation_date,
+                                                      email,
+                                                      country)
+                            VALUES(%s, %s, %s, %s, %s, %s, %s) RETURNING id_admin"""
+        ,
+        "admin_moderator":  """INSERT INTO admin_moderator(login,
+                                                          id_photo,
+                                                          password,
+                                                          creation_date,
+                                                          email,
+                                                          country)
+                            VALUES(%s, %s, %s, %s, %s, %s, %s) RETURNING id_admin"""
+        ,
+        "comment":          """INSERT INTO comment(login,
+                                                  add_date,
+                                                  content)
+                            VALUES(%s, %s, %s) RETURNING id_comment"""
+        ,
+        "post":             """INSERT INTO post(id_place,
+                                               login,
+                                               id_comment,
+                                               add_date,
+                                               id_photo,
+                                               review)
+                            VALUES(%s, %s, %s, %s, %s, %s) RETURNING id_post"""
+        ,
+        "interest":         """INSERT INTO interest(id_attraction,
+                                                   login)
+                            VALUES(%s, %s) RETURNING id_interest"""
+        ,
+        "report_user":      """INSERT INTO report_user(id_admin,
+                                                      login,
+                                                      add_date,
+                                                      reason,
+                                                      answer)
+                            VALUES(%s, %s, %s, %s, %s) RETURNING id_report"""
+        ,
+        "report_post":      """INSERT INTO report_post(login,
+                                                      id_post,
+                                                      id_admin,
+                                                      add_date,
+                                                      reason,
+                                                      answer)
+                            VALUES(%s, %s, %s, %s, %s, %s) RETURNING id_report"""
+        ,
+        "report_comment":   """INSERT INTO report_comment(login,
+                                                        id_comment,
+                                                        id_admin,
+                                                        add_date,
+                                                         reason,
+                                                         answer)
+                            VALUES(%s, %s, %s, %s, %s, %s) RETURNING id_report"""
+        ,
+        "visit":            """INSERT INTO visit(arrival_date,
+                                                login,
+                                                id_place,
+                                                departure_date)
+                            VALUES(%s, %s, %s, %s)"""
+    }
+
+    return commands[key]
 
